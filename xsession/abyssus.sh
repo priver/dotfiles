a1f4202e764fe6e2cbbdbd97df3e54eb2b8c949e
Mikhail Priver <m.priver@gmail.com>
2013-10-07 22:21:19 +0400

    Add Xsession

---
+++ xsession/abyssus.sh
@@ -0,0 +1,4 @@
+# Razer Abyssus mouse acceleration
+xinput --set-prop 'Razer  Razer Abyssus' 'Device Accel Profile' -1
+xinput --set-prop 'Razer  Razer Abyssus' 'Device Accel Constant Deceleration' 1.3
+xinput --set-prop 'Razer  Razer Abyssus' 'Device Accel Adaptive Deceleration' 1
