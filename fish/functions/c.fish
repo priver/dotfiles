37e2252a467c2f79c467a10e1e2eaf51b03e5bc0
Mikhail Priver <m.priver@gmail.com>
2021-10-21 14:16:40 +0300

    update

--- fish/functions/c.fish
+++ fish/functions/c.fish
@@ -1,6 +1,6 @@
-function p --description "Quick cd into $PROJECTS"
+function c --description "Quick open project in vscode"
   set -l dir_path (fd --max-depth=1 --type=directory --base-directory=$PROJECTS | fzf --reverse --height 35% --nth 1)
   if test -n "$dir_path"
-    cd $PROJECTS/$dir_path
+    code $PROJECTS/$dir_path
   end
 end
--- fish/functions/h.fish
+++ fish/functions/h.fish
@@ -1,3 +1,6 @@
 function h --description "Quick cd into $HOME"
-  cd $HOME/(fd --max-depth=1 --type=directory --base-directory=$HOME | fzf --reverse --height 35% --nth 1)
+  set -l dir_path (fd --max-depth=1 --type=directory --base-directory=$HOME | fzf --reverse --height 35% --nth 1)
+  if test -n "$dir_path"
+    cd $HOME/$dir_path
+  end
 end
